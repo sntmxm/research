292cf9e7c2c5803045284f4f23e8846e2206603d
Max Schastnyi <maksch-ipt25@lll.kpi.ua>
2023-06-24 22:52:11 +0300

    Create test

---
+++ C++Research/ResearchTests/test.cpp
@@ -0,0 +1,53 @@
+#include "pch.h"
+#include <gtest/gtest.h>
+#include "../C++Research/C++Research1.cpp"
+
+
+class MyTestFixture : public ::testing::Test {
+protected:
+    void SetUp() override {
+        
+    }
+
+    void TearDown() override {
+        
+    }
+};
+
+
+TEST(MyTestCase, ValueFunctionTest) {
+    int x = 5;
+    Value(x);
+    EXPECT_EQ(5, x); 
+}
+
+TEST(MyTestCase, ReferenceFunctionTest) {
+    int y = 5;
+    Reference(y);
+    EXPECT_EQ(10, y); 
+}
+
+TEST(MyTestCase, MemoryLeakTest) {
+    
+    SUCCEED();
+}
+
+
+TEST_F(MyTestFixture, MyClassStackTest) {
+    MyClass objStack;
+    
+    ASSERT_TRUE(true); 
+}
+
+TEST_F(MyTestFixture, MyClassHeapTest) {
+    MyClass* objHeap = new MyClass();
+    
+    ASSERT_TRUE(true); 
+
+    delete objHeap; 
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
